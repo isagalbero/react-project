90d28e6694647ce88df6582a0a4602821055fdf2
isagalbero <isagalbero@gmail.com>
2018-11-04 16:55:36 -0200

    build folder created and folder dist been replaced and deployed on firebase

---
+++ dist/precache-manifest.ec55e6c2b2ae3ef42131cb56df99332d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bbd26d3c62f2adcd912e",
+    "url": "/static/js/main.bbd26d3c.chunk.js"
+  },
+  {
+    "revision": "f4f959586d347341473f",
+    "url": "/static/js/1.f4f95958.chunk.js"
+  },
+  {
+    "revision": "bbd26d3c62f2adcd912e",
+    "url": "/static/css/main.c027a013.chunk.css"
+  },
+  {
+    "revision": "f21d926d242bbebd02d1ce2199e8e1da",
+    "url": "/index.html"
+  }
+];
